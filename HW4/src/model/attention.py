a432554149f68ea3a3872544faec0c4b96110521
Raghuveer Chanda <raghuveer.chanda@gmail.com>
2018-04-23 03:01:38 -0400

    decoder and attention in progress

--- HW4/src/model/attention.py
+++ HW4/src/model/attention.py
@@ -4,17 +4,21 @@ import torch.nn.functional as F
 
 
 class Attention(nn.Module):
-	def __init__(self, mask=None, hidden_size, key_size, value_size, output_size):
+	def __init__(self, hidden_size, key_size, value_size, output_size):
 		super(Attention, self).__init__()
-		self.mask = mask
+		self.mask = None
 
 		self.linear_query = nn.Linear(hidden_size, key_size)
 		self.linear_out = nn.Linear(hidden_size+value_size, output_size)
 
 
+	def set_mask(mask):
+		self.mask = mask
+
+
 	def forward(self, outputs, encoder_keys, encoder_values):
-		# outputs = (batch_size, output_len, hidden_size)
-		# query = (batch_size, output_len, key_size)
+		# outputs = (batch_size, 1, hidden_size)
+		# query = (batch_size, 1, key_size)
 		# keys = (input_len, batch_size, key_size)
 		# values = (input_len, batch_size, value_size)
 
@@ -28,19 +32,19 @@ class Attention(nn.Module):
 		keys = keys.permute(1, 2, 0)
 		values = values.permute(1, 0, 2)
 		attention = torch.bmm(query, keys)
-		# attention = (batch_size, output_len, input_len)
+		# attention = (batch_size, 1, input_len)
 
 		if self.mask is not None:
 			attention.data.masked_fill_(self.mask, -float('inf'))
 
 		attention_weights = F.softmax(attention.view(-1, input_len)).view(batch_size, -1, input_len)
 		context = torch.bmm(attention_weights, values)
-		#context = (batch_size, output_len, value_size)
+		#context = (batch_size, 1, value_size)
 
 		combined = torch.cat((context, outputs), dim=2)
-		# combined = (batch_size, output_len, hidden_size+value_size)
+		# combined = (batch_size, 1, hidden_size+value_size)
 
-		outputs = F.tanh(self.linear_out(combined).view(-1, batch_size, output_size))
+		outputs = F.tanh(self.linear_out(combined).view(batch_size, -1, output_size))
 		
-		return context, outputs
+		return outputs, context
 
