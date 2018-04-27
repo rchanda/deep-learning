b432539bc2603bf6afe739f428c14dd190f73c0b
Ubuntu <ubuntu@ip-172-31-12-137.us-east-2.compute.internal>
2018-04-27 07:43:23 +0000

    pred gen

--- HW4/src/evaluator/predictor.py
+++ HW4/src/evaluator/predictor.py
@@ -8,14 +8,15 @@ import itertools
 from data.language import Lang
 from data.dataloader import SpeechDataLoader
 from data.dataset import SpeechDataset
-
+import pdb
 class Predictor:
-    def __init__(self, model):
+    def __init__(self, model, lang):
         super(Predictor, self).__init__()
         self.model = model
+        self.lang = lang
 
 
-    def predict(self, test_dataloader):
+    def predict(self, test_dataloader, outFile):
         self.model.eval()
 
         num_batches = len(test_dataloader.dataloader)
@@ -30,7 +31,13 @@ class Predictor:
 
             batch_size = target_variables.size(0)
             decoder_outputs, ret_dict = self.model(input_variables, input_lengths, target_variables)
-            print(ret_dict['sequence'])
+
+            for i in range(batch_size):
+                length = ret_dict['length'][i]-1
+                #pdb.set_trace()
+                tgt_id_seq = [ret_dict['sequence'][di][i].item() for di in range(length)]
+                tgt_seq = self.lang.indices2items(tgt_id_seq)
+                outFile.write(''.join(tgt_seq)+'\n')
 
 
 def _test():
@@ -47,11 +54,13 @@ def _test():
     las = torch.load('../models1/0model.pt', map_location=lambda storage, loc: storage)
     las.teacher_forcing_ratio = 0.0
     las = las.cuda()
-    predictor = Predictor(las)
+    predictor = Predictor(las, lang)
     
-    predictor.predict(test_dataloader)
+    outFile = open('predictions.txt', 'w')
+    predictor.predict(test_dataloader, outFile)
 
 
 if __name__ == "__main__":
     _test()
+
 
